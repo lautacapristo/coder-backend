ccca024608cea5e66ec6c567673aabacdd030008
Lautaro Capristo <lautarocapristo@outlook.com>
2022-02-23 17:44:44 -0300

    duda

--- primerarchivo.js
+++ primerarchivo.js
@@ -1,45 +1,45 @@
-// class Contador {
-//     constructor(responsable) {
-//         this.responsable = responsable;
-//         this.contador = 0;
-//     }
-//     static contadorGeneral = 0;
-//     obtenerResponsable() {
-//         console.log(this.responsable)
-//     }
-//     obtenerCuentaIndividual() {
-//         console.log(this.contador);
-//     }
-//     obtenerCuentaGlobal() {
-//         console.log(Contador.contadorGeneral)
-//     }
-//     contar() {
-//         Contador.contadorGeneral++;
-//         this.contador++
-//     }
-// }
-
-// let contador1 = new Contador ("Marce");
 
 class Usuario {
-    constructor(nombre, apellido, libros, mascotas){
+    constructor(nombre, apellido, libros, mascotas) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.libros = libros;
         this.mascotas = mascotas;
+        
     }
 
     getName = () => {
-        console.log(this.nombre +' ' + this.apellido)
-    } 
+        console.log(`${this.nombre} ${this.apellido}`)
+    }
+    addMascota = () => {
+        this.mascotas.push("Edwincito")
+    }
+    countMascotas = () => {
+let count = this.mascotas.length()
+console.log(count)
+    }
+    addBook = () => {
+this.libros.push({nombre: "El Principito", autor: " Antoine de Saint-Exupery"})
+    }
+    getBookNames = () => {
+     this.libros.map(libro => libro.nombre)
+   
+    }
 }
 
-// const usuario1 = new Usuario("Lautaro", "Capristo")
 
-const usuario1 = new Usuario ("Lautaro", "Capristo",  [ {
-    nombre:"Los ojos del perro siberiano",  fecha: 1998 
-}, {  nombre:"Quien le teme a Demetrio Latov",  fecha: 2004} ], 
-["Sasha,", "Edwin"])
+const usuario1 = new Usuario("Lautaro", "Capristo", [{
+        nombre: "Los ojos del perro siberiano",
+        autor: "Antonio Santa Ana"
+    }, {
+        nombre: "Quien le teme a Demetrio Latov",
+        autor: "Durini Angeles"
+    }],
+    ["Sasha,", "Edwin"])
 
-console.log(usuario1)
+
 usuario1.getName();
+usuario1.addMascota();
+usuario1.addBook();
+usuario1.getBookNames();
+console.log(usuario1)
